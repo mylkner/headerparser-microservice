50f60afca8c76764421f2a01f8c6afd5fda59216
mylkner <mylofaulkner27@gmail.com>
2024-03-29 21:34:18 +0100

    set trust proxy true

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 require("dotenv").config();
 var express = require("express");
 var app = express();
+app.set("trust proxy", true);
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
